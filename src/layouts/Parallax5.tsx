ba458d1cdd8b56afe07a9579d4aa76fc21afccdc
Chris <Capistranc@gmail.com>
2021-06-22 18:40:51 -0700

    parallax layout done

--- src/layouts/Parallax5.tsx
+++ src/layouts/Parallax5.tsx
@@ -33,19 +33,6 @@ export const Parallax5 = ({ components, bgImages }) => {
   return (
     <>
       <StickyNavHeader position="fixed" />
-
-      {/* <Box
-        position="fixed"
-        top="0"
-        left="0"
-        color="white"
-        zIndex="999"
-        sx={{ "mix-blend-mode": "difference" }}
-      >
-        <Heading as="h1" color="white">
-          TEESTESTTEESTESTTEESTEST
-        </Heading>
-      </Box> */}
       <Stack as="main" height="100vh" width="100vw">
         <ParallaxBox bgImage={bgImages[0]} minHeight="100%">
           {components[0]}
