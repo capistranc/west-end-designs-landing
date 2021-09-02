7f894bb7aa85e9daf65c1329d4e6ed148b712bbd
Chris <Capistranc@gmail.com>
2021-09-01 20:45:33 -0700

    accidently a ,

--- src/components/Footer/SocialMediaLinks.tsx
+++ src/components/Footer/SocialMediaLinks.tsx
@@ -17,64 +17,63 @@ export enum SocialMedia {
   tiktok,
 }
 
-
-const SocialMediaLink = ({socialMediaType, href,, ...props}) => {
+const SocialMediaLink = ({ socialMediaType, href, ...props }) => {
   const icons = {
-    github: {label: "Github", icon: (props) =>  <FaGithub {...props} />},
-    linkedin: {label: "LinkedIn", icon: (props) =>  <FaLinkedin {...props} />},
-    instagram: {label: "Instagram", icon: (props) =>  <FaInstagram {...props} />},
-    facebook: {label: "Facebook", icon: (props) =>  <FaGithub {...props} />},
-    twitter: {label: "Twitter", icon: (props) =>  <FaTwitter {...props} />},
+    github: { label: "Github", icon: (props) => <FaGithub {...props} /> },
+    linkedin: { label: "LinkedIn", icon: (props) => <FaLinkedin {...props} /> },
+    instagram: {
+      label: "Instagram",
+      icon: (props) => <FaInstagram {...props} />,
+    },
+    facebook: { label: "Facebook", icon: (props) => <FaGithub {...props} /> },
+    twitter: { label: "Twitter", icon: (props) => <FaTwitter {...props} /> },
   };
 
-
-  return <NextLink href={href} passHref>
-  <IconButton
-    as="a"
-    href="#"
-    aria-label={icons[socialMediaType].label}
-    icon={icons[socialMediaType].icon(props)}
-  />
-</NextLink>
-
-
-
+  return (
+    <NextLink href={href} passHref>
+      <IconButton
+        as="a"
+        href="#"
+        aria-label={icons[socialMediaType].label}
+        icon={icons[socialMediaType].icon(props)}
+      />
+    </NextLink>
+  );
 };
 
 export const SocialMediaLinks = (props: ButtonGroupProps) => {
-
-  const socialLinks = [{name: 'github', href: 'https://linkedin.com/'}]
+  const socialLinks = [{ name: "github", href: "https://linkedin.com/" }];
 
   socialLinks.map((item) => {
-
-    return SocialMediaLink({socialMediaType: item.name, href: item.href, })
-  })
+    return SocialMediaLink({ socialMediaType: item.name, href: item.href });
+  });
 
   return (
-  <ButtonGroup variant="ghost" color="gray.600" {...props}>
-    <NextLink href="https://linkedin.com/" passHref>
-      <IconButton
-        as="a"
-        href="#"
-        aria-label="LinkedIn"
-        icon={<FaLinkedin size="20px" />}
-      />
-    </NextLink>
-    <NextLink href="https://github.com/" passHref>
-      <IconButton
-        as="a"
-        href="#"
-        aria-label="GitHub"
-        icon={<FaGithub fontSize="20px" />}
-      />
-    </NextLink>
-    <NextLink href="https://twitter.com" passHref>
-      <IconButton
-        as="a"
-        href="#"
-        aria-label="Twitter"
-        icon={<FaTwitter fontSize="20px" />}
-      />
-    </NextLink>
-  </ButtonGroup>
-)}
+    <ButtonGroup variant="ghost" color="gray.600" {...props}>
+      <NextLink href="https://linkedin.com/" passHref>
+        <IconButton
+          as="a"
+          href="#"
+          aria-label="LinkedIn"
+          icon={<FaLinkedin size="20px" />}
+        />
+      </NextLink>
+      <NextLink href="https://github.com/" passHref>
+        <IconButton
+          as="a"
+          href="#"
+          aria-label="GitHub"
+          icon={<FaGithub fontSize="20px" />}
+        />
+      </NextLink>
+      <NextLink href="https://twitter.com" passHref>
+        <IconButton
+          as="a"
+          href="#"
+          aria-label="Twitter"
+          icon={<FaTwitter fontSize="20px" />}
+        />
+      </NextLink>
+    </ButtonGroup>
+  );
+};
