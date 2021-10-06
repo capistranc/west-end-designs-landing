2544b53e9307c177d9a0be807779028ba0c436d4
Chris <Capistranc@gmail.com>
2021-10-06 06:44:52 -0700

    cleanup

--- src/theme/components/index.ts
+++ src/theme/components/index.ts
@@ -1,5 +1,4 @@
 import { Heading } from "./Heading";
 import { Button } from "./Button";
-import { FormLabel } from "./FormLabel";
-import { Box } from "./Box";
-export default { Heading, Button, FormLabel, Box };
+
+export default { Heading, Button };
