6dfa016d3d52b3031c7a82f7bff6bea65ef8344c
Chris <Capistranc@gmail.com>
2021-11-20 20:08:16 -0800

    blah next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 module.exports = {
+  env: {
+    EmailJS_ID: "user_4LtQ3LRB7J6d60KxeHQuY",
+    EmailJS_Token: "089dcdd07fb34862435ba6141d325ff9",
+    GA_ID: "G-F4GXGE5JT6",
+  },
   typescript: {
     // !! WARN !!
     // Dangerously allow production builds to successfully complete even if
