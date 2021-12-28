470afc8a87d1533620abb27e24eb5e73c20bd7dd
Chris Capistran <chriscapistran@Chriss-MacBook-Pro.local>
2021-12-28 01:28:54 -0800

    oauth

--- src/pages/api/oauth.ts
+++ src/pages/api/oauth.ts
@@ -1,23 +1,25 @@
-const { DCLIENT_ID, DCLIENT_SECRET } = process.env;
+require("dotenv").config();
+
+const { DISCORD_CLIENT_ID, DISCORD_CLIENT_SECRET } = process.env;
 
 export default function handler(req, res) {
-  let ret = {};
-  if (req.method === "POST") {
-    console.log(req.body);
-    const { code, redirect_uri } = req.body;
+  // let ret = {};
+  // if (req.method === "POST") {
+  console.log(req.body);
+  const { code, redirect_uri } = req.body;
 
-    // Process a POST request
-    const ret = {
-      client_id: DCLIENT_ID,
-      client_secret: DCLIENT_SECRET,
-      grant_type: "authorization_code",
-      code: code,
-      redirect_uri,
-    };
+  // Process a POST request
+  const ret = {
+    client_id: DISCORD_CLIENT_ID,
+    client_secret: DISCORD_CLIENT_SECRET,
+    grant_type: "authorization_code",
+    code: code,
+    redirect_uri,
+  };
 
-    res.send(ret);
-  } else {
-    res.send(ret);
-    // Handle any other HTTP method
-  }
+  res.send(ret);
+  // } else {
+  //   res.send(ret);
+  // Handle any other HTTP method
+  // }
 }
